0462008ffdab389d9bde17194c4e284fef57e8f3
MyNameIsYg <hand1b10@mbjcomp.org.il>
2023-03-15 18:21:20 +0200

    change10

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -1,4 +1,22 @@
-﻿
+﻿CREATE TABLE [dbo].[Handyman]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY,
+	[FirstName] nvarchar(20) not null,
+	[LastName] nvarchar(20) not null,
+	[City] int FOREIGN KEY REFERENCES Cities(Id) not null,
+	[PhoneNumber] varchar(10) not null,
+	[EmailAddress] varchar(225) not null
+)
+
+CREATE TABLE [dbo].[Handyman]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY,
+	[FirstName] nvarchar(20) not null,
+	[LastName] nvarchar(20) not null,
+	[City] int FOREIGN KEY REFERENCES Cities(Id) not null,
+	[PhoneNumber] varchar(10) not null,
+	[EmailAddress] varchar(225) not null
+)
 
 ALTER TABLE Handyman
 ADD Password_login nvarchar(100);
