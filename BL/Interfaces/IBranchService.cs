9dc873b997899b8ba8789baa7d6eb71004163741
NameRschool <hand1b20@mbjcomp.org.il>
2023-03-19 15:09:54 +0200

    change15

---
+++ BL/Interfaces/IBranchService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BL.DTO;
+
+namespace BL.Interfaces
+{
+    internal interface IBranchService:IService<BranchDTO>
+    {
+    }
+}
