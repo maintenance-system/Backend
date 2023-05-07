7f33e10af9ac97e56270c553ce123067c966674f
MyNameIsYg <hand1b10@mbjcomp.org.il>
2023-05-07 15:43:40 +0300

    change32

--- BL/DTO/LogIn/UserRoleDTO.cs
+++ BL/DTO/LogIn/UserRoleDTO.cs
@@ -7,11 +7,11 @@ public partial class UserRoleDTO
 {
     public int Id { get; set; }
 
-    public string UserId { get; set; }
+    public string User { get; set; }
 
-    public string RoleId { get; set; }
+    public string Role { get; set; }
 
-  /*  public virtual RoleDTO Role { get; set; } = null!;
+   /* public virtual RoleDTO Role { get; set; } = null!;
 
     public virtual UserDTO User { get; set; } = null!;*/
 }
