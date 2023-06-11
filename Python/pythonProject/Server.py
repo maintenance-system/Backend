b126592926ba7b86dfc663c7a15a6c63f97891ad
Yael Greenberg <117977974+MyNameIsYg@users.noreply.github.com>
2023-06-11 20:07:06 +0300

    Add a server in python

---
+++ Python/pythonProject/Server.py
@@ -0,0 +1,26 @@
+
+# Importing flask module in the project is mandatory
+# An object of Flask class is our WSGI application.
+from flask import Flask
+import virtualenv
+# Flask constructor takes the name of
+# current module (__name__) as argument.
+from main import sortText
+
+app = Flask(__name__)
+
+# The route() function of the Flask class is a decorator,
+# which tells the application which URL should call
+# the associated function.
+@app.route('/')
+# ‘/’ URL is bound with hello_world() function.
+def hello_world():
+    return sortText()
+
+# main driver function
+if __name__ == '__main__':
+
+    # run() method of Flask class runs the application
+    # on the local development server.
+    #app.debug = True
+    app.run()
