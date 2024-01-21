f9f53dca94d284d37efc4ede93a7500e857c6730
MyNameIsYg <hand1b10@mbjcomp.org.il>
2024-01-21 16:44:22 +0200

    All the changes, its not reload

---
+++ BL/Interfaces/IFilesService.cs
@@ -0,0 +1,14 @@
+﻿using BL.DTO;
+using BL.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BL.Interfaces;
+
+public interface IFilesService : IService<FilesDTO>
+{
+    Task<List<FilesDTO>> GetByStatusAsync(Status.FileStatus fileStatus);
+}
---
+++ DAL/Implementation/FilesRepository.cs
@@ -0,0 +1,57 @@
+﻿using DAL.DataObjects;
+using DAL.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Implementation;
+
+public class FilesRepository : IFilesRepository
+{
+
+    DBContext context;
+    public FilesRepository(DBContext context)
+    {
+        this.context = context;
+    }
+
+    public async Task<int> CreateAsync(Files item)
+    {
+        var result = context.Files.Add(item);
+        await context.SaveChangesAsync();
+        return result.Entity.Id;
+    }
+
+    public async Task<bool> DeleteAsync(int id)
+    {
+        var result = context.Files.Where(p => p.Id == id).FirstOrDefault();
+        if (result != null)
+        {
+            context.Files.Remove(result);
+            await context.SaveChangesAsync();
+            return true;
+        }
+        return false;
+    }
+
+    public async Task<List<Files>> GetAllAsync()
+    {
+        return await context.Files
+          .ToListAsync<Files>();
+    }
+
+    public async Task<List<Files>> GetByStatusAsync(string status)
+    {
+        return await context.Files
+            .Where(file => file.Status.Equals(status))
+            .ToListAsync<Files>();
+    }
+
+    public bool Update(Files item)
+    {
+        throw new NotImplementedException();
+    }
+}
